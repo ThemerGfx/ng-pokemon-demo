33051c62ee88110eb1254d6ee0ccc21fdeadea8c
ThemerGfx <thamer.gouider@gmail.com>
2024-02-07 14:14:21 +0100

    les pipes et les routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { BorderCardDirective } from './border-card.directive';
+import { BorderCardDirective } from "./border-card.directive";
+import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
+import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
+import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, BorderCardDirective],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
