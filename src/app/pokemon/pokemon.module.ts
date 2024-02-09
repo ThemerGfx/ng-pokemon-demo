fb032c9002c7f3dfd33f27d88eb279c1270c3310
ThemerGfx <thamer.gouider@gmail.com>
2024-02-09 17:34:23 +0100

    requette http et rxj

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -5,8 +5,17 @@ import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.componen
 import { BorderCardDirective } from "./border-card.directive";
 import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
 import { RouterModule, Routes } from "@angular/router";
+import { PokemonService } from "./pokemon.service";
+import { FormsModule } from "@angular/forms";
+import { PokemonFormComponent } from "./pokemon-form/pokemon-form.component";
+import { EditPokemonComponent } from "./edit-pokemon/edit-pokemon.component";
+import { AddPokemonComponent } from "./add-pokemon/add-pokemon.component";
+import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
+import { LoaderComponent } from './loader/loader.component';
 
 const pokemonRoutes: Routes = [
+  { path: "edit/pokemon/:id", component: EditPokemonComponent },
+  { path: "pokemon/add", component: AddPokemonComponent },
   { path: "pokemons", component: ListPokemonComponent },
   { path: "pokemon/:id", component: DetailPokemonComponent },
 ];
@@ -17,7 +26,13 @@ const pokemonRoutes: Routes = [
     DetailPokemonComponent,
     BorderCardDirective,
     PokemonTypeColorPipe,
+    PokemonFormComponent,
+    EditPokemonComponent,
+    AddPokemonComponent,
+    SearchPokemonComponent,
+    LoaderComponent,
   ],
-  imports: [CommonModule, RouterModule.forChild(pokemonRoutes)],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(pokemonRoutes)],
+  providers: [PokemonService],
 })
 export class PokemonModule {}
